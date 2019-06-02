050b915e5a403bdfc1889074774ffa0ba701ae93
TGYK <tgyk@tgyk.net>
2019-06-02 17:33:07 -0500

    Change directory to include repo name

--- weather/predict/schedule_all.sh
+++ weather/predict/schedule_all.sh
@@ -7,11 +7,11 @@
 
 # Update Satellite Information
 
-wget -qr https://www.celestrak.com/NORAD/elements/weather.txt -O /home/pi/weather/predict/weather.txt
-grep "NOAA 15" /home/pi/weather/predict/weather.txt -A 2 > /home/pi/weather/predict/weather.tle
-grep "NOAA 18" /home/pi/weather/predict/weather.txt -A 2 >> /home/pi/weather/predict/weather.tle
-grep "NOAA 19" /home/pi/weather/predict/weather.txt -A 2 >> /home/pi/weather/predict/weather.tle
-grep "METEOR-M 2" /home/pi/weather/predict/weather.txt -A 2 >> /home/pi/weather/predict/weather.tle
+wget -qr https://www.celestrak.com/NORAD/elements/weather.txt -O /home/pi/rpiWX/weather/predict/weather.txt
+grep "NOAA 15" /home/pi/rpiWX/weather/predict/weather.txt -A 2 > /home/pi/rpiWX/weather/predict/weather.tle
+grep "NOAA 18" /home/pi/rpiWX/weather/predict/weather.txt -A 2 >> /home/pi/rpiWX/weather/predict/weather.tle
+grep "NOAA 19" /home/pi/rpiWX/weather/predict/weather.txt -A 2 >> /home/pi/rpiWX/weather/predict/weather.tle
+grep "METEOR-M 2" /home/pi/rpiWX/weather/predict/weather.txt -A 2 >> /home/pi/rpiWX/weather/predict/weather.tle
 
 
 
@@ -22,7 +22,7 @@ for i in `atq | awk '{print $1}'`;do atrm $i;done
 
 #Schedule satellite passes with their frequencies, in order of priority:
 
-/home/pi/weather/predict/schedule_satellite.sh "METEOR-M 2" 137.9000
-/home/pi/weather/predict/schedule_satellite.sh "NOAA 19" 137.1000
-/home/pi/weather/predict/schedule_satellite.sh "NOAA 18" 137.9125
-/home/pi/weather/predict/schedule_satellite.sh "NOAA 15" 137.6200
+/home/pi/rpiWX/weather/predict/schedule_satellite.sh "METEOR-M 2" 137.9000
+/home/pi/rpiWX/weather/predict/schedule_satellite.sh "NOAA 19" 137.1000
+/home/pi/rpiWX/weather/predict/schedule_satellite.sh "NOAA 18" 137.9125
+/home/pi/rpiWX/weather/predict/schedule_satellite.sh "NOAA 15" 137.6200
